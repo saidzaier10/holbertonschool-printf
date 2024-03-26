08d14daf3e7841ea3943fa1b09c3b922aec5fb69
SaidZaier <8770@holbertonstudents.com>
2024-03-26 15:28:49 +0100

    my first commit

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - prints output according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed (excluding the null byte)
+ */
+
+int _printf(const char *format, ...)

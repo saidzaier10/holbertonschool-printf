4a97cbffba05e82a07761d075fb8bbf161334fc3
SaidZaier <8770@holbertonstudents.com>
2024-03-29 16:16:38 +0100

    fix betty

--- print_percent.c
+++ print_percent.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * print_percent - Prints a percent character
+ * This function prints a percent character.
+ * @args: A list of arguments (unused)
+ * Return: Always returns 1
+ */
 
 int print_percent(va_list args)
 {

8883ae25cb46f4d95a2e2d107d0528c030cf284d
SaidZaier <8770@holbertonstudents.com>
2024-03-29 15:36:41 +0100

    add printf function

--- _printf.c
+++ _printf.c
@@ -1,12 +1,51 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-
 /**
- * _printf - prints output according to a format
- * @format: format string
- *
- * Return: number of characters printed (excluding the null byte)
- */
-
+* _printf - Prints formatted output to stdout
+* @format: The format string
+* @...: Optional arguments
+*
+* Return: The number of characters printed (excluding the null byte)
+*/
 int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	format_specifier_t specifiers[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_dec_integer},
+		{'i', print_dec_integer},
+		{'\0', NULL}
+	};
+
+	int index = 0;
+
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			while (specifiers[index].specifier)
+			{
+				if (*format == specifiers[index].specifier)
+				{
+					count += specifiers[index].ptr_function(args);
+					break;
+				}
+				index++;
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
